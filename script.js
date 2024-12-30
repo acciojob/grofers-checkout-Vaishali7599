35259cfca6fc415783cd594d379cf385491be855
Vaishali7599 <140495282+Vaishali7599@users.noreply.github.com>
2024-12-30 22:56:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,9 +3,43 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	const prices = document.querySelectorAll(".price");
+	let sum = 0;
+	prices.forEach(price => {
+		sum += Number(price.innerText.replace("$", ""));
+	});
+	const totalRow = document.createElement("tr")
+	totalRow.innerHTML = `<td>total</td>
+	<td>${sum}</td>
+	`;
+	document.querySelector("table").appendChild(totalRow);
+	
 };
 
 getSumBtn.addEventListener("click", getSum);
+
+
+// const getSumBtn = document.createElement("button");
+// getSumBtn.append("Get Total Price");
+// document.body.appendChild(getSumBtn);
+
+// const getSum = () => {
+//     //Add your code here
+//     const prices = document.querySelectorAll(".price");
+//     let totalPrice = 0;
+//     prices.forEach(price => {
+//         totalPrice += Number(price.innerText.replace("$", ""));
+//     });
+//     // alert(`Total price: $${totalPrice.toFixed(2)}`);
+//     // console.log(totalPrice);
+//     const totalRow = document.createElement("tr");
+//     totalRow.innerHTML = `<td>total</td>
+//             <td id="ans">${totalPrice}</td>
+//     `;
+//     document.querySelector("table").appendChild(totalRow);
+
+// };
+
+// getSumBtn.addEventListener("click", getSum);
+
 
